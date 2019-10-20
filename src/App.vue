6f95f1652cf2fa537081e5e7dce1ac02ed5bc9e2
PammyLo <pamela@tuminds.com>
2019-10-20 15:23:10 +0100

    added display details on select

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div>
     <h1>Ghibli World</h1>
     <film-list :films="films"></film-list>
+    <film-details v-if="selectedFilm" :film="selectedFilm"></film-details>
   </div>
 </template>
 
 <script>
 import FilmList from '@/components/FilmList.vue';
+import FilmDetails from '@/components/FilmDetails.vue';
+import {eventBus} from '@/main.js'
 
 export default {
   name: "app",
@@ -20,10 +23,15 @@ export default {
   mounted() {
     fetch('https://ghibliapi.herokuapp.com/films')
     .then(result => result.json())
-    .then(data => this.films = data)
+    .then(data => this.films = data);
+
+    eventBus.$on('film-selected', (film) => {
+      this.selectedFilm = film;
+    });
   },
   components: {
-    'film-list': FilmList
+    'film-list': FilmList,
+    'film-details': FilmDetails
   }
 }
 </script>
--- src/components/FilmDetails.vue
+++ src/components/FilmDetails.vue
@@ -1,5 +1,12 @@
 <template lang="html">
-
+  <div>
+    <h2>Film details</h2>
+    <h3>{{ film.title }}</h3>
+    <p>{{ film.description }}</p>
+    <p>{{ film.director }}</p>
+    <p>{{ film.producer }}</p>
+    <p>{{ film.release_date }}</p>
+  </div>
 </template>
 
 <script>
